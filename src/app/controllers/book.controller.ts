71ed78a11d158734de5c6fd2f980b17b98ca399d
Imtiaz <mdimtiazzihad@gmail.com>
2025-06-22 20:52:06 +0600

    Implement CRUD operations for books: add, update, retrieve, and delete functionalities

--- src/app/controllers/book.controller.ts
+++ src/app/controllers/book.controller.ts
@@ -43,6 +43,28 @@ bookRoutes.get("/books", async (req: Request, res: Response) => {
   }
 });
 
+// Get a Book by ID
+
+bookRoutes.get("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const book = await Book.findById(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Book retrieved successfully!",
+      data: book,
+    });
+  } catch (error) {
+    console.error("Error fetching book:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to retrieve book",
+      error: error instanceof Error ? error.message : error,
+    });
+  }
+});
+
 // Post A Book
 bookRoutes.post("/books", async (req: Request, res: Response) => {
   const bookDeatails = req.body;
@@ -56,5 +78,57 @@ bookRoutes.post("/books", async (req: Request, res: Response) => {
     });
   } catch (error) {
     res.status(500).json({ message: "Internal Server Error", error });
+  }
+});
+
+bookRoutes.put("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const updatedBookDeatils = req.body;
+    const book = await Book.findByIdAndUpdate({ _id: id }, updatedBookDeatils, {
+      new: true,
+      runValidators: true,
+    });
+    if (!book) {
+      res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Books Updated successfully!",
+      data: book,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to update books",
+      error,
+    });
+  }
+});
+
+bookRoutes.delete("/books/:bookId", async (req: Request, res: Response) => {
+  try {
+    const id = req.params.bookId;
+    const book = await Book.findByIdAndDelete({ _id: id });
+    if (!book) {
+      res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Books Deleted successfully!",
+      data: book,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete books",
+      error,
+    });
   }
 });
